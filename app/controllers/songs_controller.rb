79a6961e65b8ef5462594a12349a3ccfe088cb78
reduviidae <wiljago@gmail.com>
2019-01-16 11:12:22 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,16 +1,20 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
+    @song = Song.new
+    @artists = Artist.all
   end
 
   def create
     @song = Song.new(song_params)
-
+    byebug
     if @song.save
       redirect_to @song
     else
@@ -44,7 +48,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_id)
   end
 end
-
